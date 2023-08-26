ee2d7d529a9af5d1d4b4d02ef3a9dd6e70aa7f82
Suphakit P <me@suphakit.net>
2023-08-27 06:25:21 +0700

    feat<const>: added contents based

---
+++ src/constants/maimai_content.ts
@@ -0,0 +1,24 @@
+const maimaiContents = [
+	{
+		title: 'Virtual To LIVE',
+		date: 'Jul 29, 2023',
+		img: 'https://be.000198.xyz/file/2023-08-2623:20:11.435888+00:002023-07-29_19-16.webp'
+	},
+	{
+		title: 'ビターチョコデコレーション',
+		date: 'Jul 31, 2023',
+		img: 'https://be.000198.xyz/file/2023-08-2623:20:32.536398+00:002023-07-31_16-10.webp'
+	},
+	{
+		title: 'Bad Apple!! feat nomico',
+		date: 'Jul 31, 2023',
+		img: 'https://be.000198.xyz/file/2023-08-2623:22:56.305447+00:002023-07-31_18-03.webp'
+	},
+	{
+		title: 'INTERNET OVERDOSE',
+		date: 'Aug 1, 2023',
+		img: 'https://be.000198.xyz/file/2023-08-2623:22:59.223936+00:002023-08-01_18-36.webp'
+	},
+];
+
+export default maimaiContents;
---
+++ src/constants/main_content.ts
@@ -0,0 +1,75 @@
+const mainImage = [
+  {
+    img: "https://be.000198.xyz/file/2023-08-2623:04:47.641488+00:001684861200000.webp",
+    title: 'first time after a looong time <3',
+    date: 'May 24, 2023'
+  },
+  {
+    img: 'https://be.000198.xyz/file/2023-08-2623:04:49.338310+00:001685552400000.webp',
+    title: 'idk why im loving this girl tooo much 🥺',
+    date: 'Jun 1, 2023'
+  },
+  {
+    img: 'https://be.000198.xyz/file/2023-08-2623:05:07.301367+00:001686934800000.webp',
+    title: 'Me & U ❤️',
+    date: 'Jun 17, 2023'
+  },
+  {
+    img: 'https://be.000198.xyz/file/2023-08-2623:05:10.126562+00:001686934800001.webp',
+    title: 'i just love the time we hold our hands 🥺',
+    date: 'Jun 17, 2023'
+  },
+  {
+    img: 'https://be.000198.xyz/file/2023-08-2623:05:22.339019+00:001686934800002.webp',
+    title: 'I.F.L.Y. 🥺❤️',
+    date: 'Jun 17, 2023'
+  },
+  {
+    img: 'https://be.000198.xyz/file/2023-08-2623:05:24.495682+00:001688662800000.webp',
+    title: "Love the time I'm with u ❤️",
+    date: 'Jul 7, 2023'
+  },
+  {
+    img: 'https://be.000198.xyz/file/2023-08-2623:05:31.602564+00:001688662800001.webp',
+    title: "Just hope to be with u like this ❤️❤️",
+    date: 'Jul 7, 2023'
+  },
+  {
+    img: 'https://be.000198.xyz/file/2023-08-2623:06:01.879510+00:001688835600000.webp',
+    title: "Luv u kubb ❤️",
+    date: 'Jul 9, 2023'
+  },
+  {
+    img: 'https://be.000198.xyz/file/2023-08-2623:06:04.698078+00:001688835600001.webp',
+    title: "Hope to be with u like this in everyday 🥺",
+    date: 'Jul 9, 2023'
+  },
+  {
+    img: 'https://be.000198.xyz/file/2023-08-2623:06:16.610635+00:001688835600002.webp',
+    title: "🥺💕",
+    date: 'Jul 9, 2023'
+  },
+  {
+    img: 'https://be.000198.xyz/file/2023-08-2623:06:32.390122+00:001690588800000.webp',
+    title: "🤍💖💕",
+    date: 'Jul 29, 2023'
+  },
+  {
+    img: 'https://be.000198.xyz/file/2023-08-2623:19:47.393458+00:001690675200000.webp',
+    title: "mhuaa~ 💖💕",
+    date: 'Jul 30, 2023'
+  },
+  {
+    img: 'https://be.000198.xyz/file/2023-08-2623:06:35.323318+00:001690848000000.webp',
+    title: "The day we need to split again 😭😭",
+    date: 'Aug 1, 2023'
+  }
+]
+
+// const based = {
+//   img: '',
+//   title: '',
+//   date: ''
+// }
+
+export default mainImage
