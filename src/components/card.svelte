a086c707fb1a7176fd741f98ef3a1f4da846a707
Suphakit P <me@suphakit.net>
2023-08-28 07:45:27 +0700

    feat<pages>: added main pics page

---
+++ src/components/card.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type mainInterface from "../interface/mainInterface";
+
+  export let data: mainInterface
+</script>
+
+<div class="card w-[20rem] p-4 rounded-[10px] bg-whitesmoke text-pastel-black text-center">
+  <div class="w-full h-[18rem] rounded-[10px] border-[1px] border-solid border-pastel-black !bg-center !bg-no-repeat !bg-cover" style={`background: url(${data.img})`}></div>
+  <div>
+    <p class="text-2xl p-4 !pb-2">
+      {data.title}
+    </p>
+    <p class="p-4 !pt-2 text-[#515454] text-right">
+      {data.date}
+    </p>
+  </div>
+</div>
---
+++ src/routes/pics/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Card from "../../components/card.svelte";
+	import mainImage from "../../constants/main_content";
+</script>
+
+<section class="flex flex-row w-full md:w-[80%] mx-auto pt-16 pb-8 flex-wrap gap-4 justify-center">
+  {#each mainImage as data}
+    <Card {data} />
+  {/each}
+</section>
